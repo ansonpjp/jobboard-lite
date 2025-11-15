ae89d3ccfd661fc7b34b5761b35fe6895fc2130e
Anson Joseph <ansonpjp@gmail.com>
2025-11-15 14:06:48 +1300

    Add files via upload

---
+++ jobboard-lite/README.md
@@ -0,0 +1,2 @@
+# Sample Laravel-like Project
+Minimal structure.
---
+++ jobboard-lite/app/Models/Job.php
@@ -0,0 +1,5 @@
+<?php
+namespace App\Models;
+class Job {
+    public $title; public $description; public $company; public $location; public $salary;
+}
