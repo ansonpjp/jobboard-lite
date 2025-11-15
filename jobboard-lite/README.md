ae2b581a5b3c5848fa555077153d3ab4e8452903
Anson Joseph <ansonpjp@gmail.com>
2025-11-15 14:07:39 +1300

    Delete jobboard-lite directory

--- jobboard-lite/README.md
+++
@@ -1,2 +0,0 @@
-# Sample Laravel-like Project
-Minimal structure.
--- jobboard-lite/app/Models/Job.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace App\Models;
-class Job {
-    public $title; public $description; public $company; public $location; public $salary;
-}
